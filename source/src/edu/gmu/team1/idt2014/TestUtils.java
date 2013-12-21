fb17905d8735228ba319503a681d7cdc7a86edd8
NjaFriday <bwinter2@gmu.edu>
2013-12-21 13:43:29 -0500

    Added docs

--- source/src/edu/gmu/team1/idt2014/TestUtils.java
+++ source/src/edu/gmu/team1/idt2014/TestUtils.java
@@ -1,6 +1,10 @@
 package edu.gmu.team1.idt2014;
 
 public class TestUtils {
+	/**
+	 * @param depth
+	 * @return
+	 */
 	public static String getMethodName(int depth) {
 		StackTraceElement[] stackTraceElements = Thread.currentThread()
 				.getStackTrace();
