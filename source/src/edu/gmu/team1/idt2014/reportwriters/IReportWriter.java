272e54cd053d62605cf005f5c758cf8a60c91789
Sriram R <S.Rajaraman89@gmail.com>
2013-12-21 21:42:33 -0500

    ReportWriter
    -Working logTest method
    -Change the FILENAME when needed

--- source/src/edu/gmu/team1/idt2014/reportwriters/IReportWriter.java
+++ source/src/edu/gmu/team1/idt2014/reportwriters/IReportWriter.java
@@ -11,7 +11,7 @@ package edu.gmu.team1.idt2014.reportwriters;
  */
 public interface IReportWriter {
 
-	void logTest(String tclass, String tmethod, boolean passed, Object output,
+	void logTest(String tclass, String tmethod, boolean passed,int currentBranch, int allBranches, Object output,
 			Object[] inputs);
 
 }
