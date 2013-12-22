4a3362cb67fb35c751f83b201df48dd4c08ddbad
Akshay Karthik <akshay.karthik@gmail.com>
2013-12-22 09:20:53 -0500

    Fixed small memory leak

--- source/src/edu/gmu/team1/idt2014/logviewer/LogViewer.java
+++ source/src/edu/gmu/team1/idt2014/logviewer/LogViewer.java
@@ -25,15 +25,13 @@ import javax.swing.table.TableRowSorter;
 import javax.swing.JFileChooser;
 
 public class LogViewer {
-	
+
 	private JFrame frame;
 	private JButton loadLog;
 	private JButton runFilter;
 	private JTextField filterText;
 	private JTable table;
-	
-	private JButton clearLog;
-	
+
 	private File file;
 
 	private TableRowSorter<ReportTableModel> sorter;
@@ -75,30 +73,28 @@ public class LogViewer {
 		loadLog.setText("Load Log");
 		loadLog.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-//				Random Record Generator
-//				model.addData("d " + increment++, "t " + increment * 2,
-//						(Math.random() < 0.5 ? "passed" : "failed"), "c", "m",
-//						"b/nb", "i", "o");
-				
-				JFileChooser chooser = new JFileChooser();
-				chooser.setFileSelectionMode(chooser.FILES_ONLY);
-				int returnVal = chooser.showOpenDialog(null);
-				
-				if(returnVal == JFileChooser.APPROVE_OPTION){
-					file = chooser.getSelectedFile();
-				}
-				else{
+				// Random Record Generator
+				// model.addData("d " + increment++, "t " + increment * 2,
+				// (Math.random() < 0.5 ? "passed" : "failed"), "c", "m",
+				// "b/nb", "i", "o");
 
-				}		
-				
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				int returnVal = chooser.showOpenDialog(null);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					file = chooser.getSelectedFile();
+				} else {
+
+				}
+
 				addRecords(file);
-				
-				}			
+
+			}
 		});
-		
-		
+
 		frame.getContentPane().add(loadLog, BorderLayout.PAGE_START);
-		
+
 		JPanel bottomPanel = new JPanel();
 		frame.getContentPane().add(bottomPanel, BorderLayout.PAGE_END);
 		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
@@ -139,23 +135,20 @@ public class LogViewer {
 		bottomPanel.add(runFilter);
 		frame.getRootPane().setDefaultButton(runFilter);
 	}
-	
-	/* TODO:
-	 * 		 1. Add expressions for class, method, branches, input, and output.
-	 * 		 2. Add green/red background for pass/fail.
-	 * 		 3. Add Upper/lowercase filtering fix on logviewer.
-	 * 		 4. Add clear button for the log
-	 * 		 5. Clean up.
+
+	/*
+	 * TODO: 1. Add expressions for class, method, branches, input, and output.
+	 * 2. Add green/red background for pass/fail. 3. Add Upper/lowercase
+	 * filtering fix on logviewer. 4. Add clear button for the log 5. Clean up.
 	 */
-	private void addRecords(File file){
-		
+	private void addRecords(File file) {
+
 		final String IGNORE_LINE_PATTERN = "\\~";
 		final String DATE_PATTERN = "\\d{1,2}\\/\\d{1,2}\\/\\d{4}";
 		final String TIME_PATTERN = "\\d{2}:\\d{2}";
 		final String PASS_PATTERN = "(passed|Passed)|(failed|Failed)|(pass|Pass)|(fail|Fail)";
-		
-		
-		//"(?:\\[[cC]\\:)([a-zA-Z0-9_\\.]*)(?:\\])"
+
+		// "(?:\\[[cC]\\:)([a-zA-Z0-9_\\.]*)(?:\\])"
 		// (?:\\[[cC]\\:) - matches [c:, ignores group ( (?: )
 		// ([a-zA-Z0-9_\\.]*) - matches repeated a-zA-Z0-9_. group
 		// (?:\\]) - matches ], ignores group ( (?: )
@@ -164,92 +157,98 @@ public class LogViewer {
 		final String BRANCH_PATTERN = "(?:\\[[bB]\\:)([a-zA-Z0-9_\\.]*)(?:\\])";
 		final String INPUT_PATTERN = "(?:\\[[iI]\\:)([a-zA-Z0-9_\\.]*)(?:\\])";
 		final String OUTPUT_PATTERN = "(?:\\[[oO]\\:)([a-zA-Z0-9_\\.]*)(?:\\])";
-		
+
 		String date, time, pass, cls, method, branches, input, output, line;
 		date = time = pass = cls = method = branches = input = output = line = "";
 		boolean flag = false;
-		
-		
-		try{
-			Scanner scan = new Scanner(file);
-			
-			
-			while((line = scan.nextLine()) != null){
-//				System.out.println(line);
-				
+
+		Scanner scan = null;
+
+		try {
+			scan = new Scanner(file);
+
+			while ((line = scan.nextLine()) != null) {
+				// System.out.println(line);
+
 				Pattern p = Pattern.compile(IGNORE_LINE_PATTERN);
 				Matcher m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					flag = true;
 					break;
 				}
-				if(flag){
+				if (flag) {
 					flag = false;
 					continue;
 				}
-				
+
 				p = Pattern.compile(DATE_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					date = m.group(0);
-//					System.out.println("Date: " + date);
+					// System.out.println("Date: " + date);
 				}
-				
+
 				p = Pattern.compile(TIME_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					time = m.group(0);
-//					System.out.println("Time: " + time);
+					// System.out.println("Time: " + time);
 				}
-				
+
 				p = Pattern.compile(PASS_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					pass = m.group(0);
-//					System.out.println("Status: " + pass + "\n");
+					// System.out.println("Status: " + pass + "\n");
 				}
-				
+
 				p = Pattern.compile(CLASS_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					cls = m.group(1);
 				}
-				
+
 				p = Pattern.compile(METHOD_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					method = m.group(1);
 				}
-				
+
 				p = Pattern.compile(BRANCH_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					branches = m.group(1);
 				}
-				
+
 				p = Pattern.compile(INPUT_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					input = m.group(1);
 				}
-				
+
 				p = Pattern.compile(OUTPUT_PATTERN);
 				m = p.matcher(line);
-				while(m.find()){
+				while (m.find()) {
 					output = m.group(1);
 				}
-				
-				if((date + time + pass + cls + method + branches + input + output).equals(""))
+
+				if ((date + time + pass + cls + method + branches + input + output)
+						.equals(""))
 					continue;
-				model.addData(date, time, pass, cls, method, branches, input, output);
+				model.addData(date, time, pass, cls, method, branches, input,
+						output);
 				date = time = pass = cls = method = branches = input = output = line = "";
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File not Found");
 		} catch (NoSuchElementException e2) {
-			
-		} catch(NullPointerException e3){
+
+		} catch (NullPointerException e3) {
 			return;
+		} finally {
+			if (!(scan == null)){
+				scan.close();
+			}
 		}
 	}
 }
