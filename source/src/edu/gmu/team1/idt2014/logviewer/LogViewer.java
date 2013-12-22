e47ef20a083c5dfe9c78c48bf415ad6940bcde86
NjaFriday <bwinter2@gmu.edu>
2013-12-22 00:48:28 -0500

    Added File Chooser for Log Viewer
    Need to finish all but date, time, and pass/fail. Extended report format
    file for testing.

--- source/src/edu/gmu/team1/idt2014/logviewer/LogViewer.java
+++ source/src/edu/gmu/team1/idt2014/logviewer/LogViewer.java
@@ -5,8 +5,13 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -20,12 +25,14 @@ import javax.swing.table.TableRowSorter;
 import javax.swing.JFileChooser;
 
 public class LogViewer {
-
+	
 	private JFrame frame;
 	private JButton loadLog;
 	private JButton runFilter;
 	private JTextField filterText;
 	private JTable table;
+	
+	private JButton clearLog;
 	
 	private File file;
 
@@ -70,6 +77,7 @@ public class LogViewer {
 		loadLog.setText("Load Log");
 		loadLog.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
+//				Random Record Generator
 //				model.addData("d " + increment++, "t " + increment * 2,
 //						(Math.random() < 0.5 ? "passed" : "failed"), "c", "m",
 //						"b/nb", "i", "o");
@@ -77,31 +85,25 @@ public class LogViewer {
 				JFileChooser chooser = new JFileChooser();
 				chooser.setFileSelectionMode(chooser.FILES_ONLY);
 				int returnVal = chooser.showOpenDialog(null);
+				
 				if(returnVal == JFileChooser.APPROVE_OPTION){
 					file = chooser.getSelectedFile();
-					System.out.println("File: " + file.getName());
 				}
 				else{
-					System.out.println("Cancelled");
-				}
+//					System.out.println("Cancelled");
+				}		
 				
-				
-				
-							
+				addRecords(file);
 				
 				/*
-				 * TODO: Load File Picker Load Report Data Add Data to Model
-				 * model.addData(Date, Time, Passed, Class, Method,
-				 * Branches/TotalBranches Input, Output, Expected Output)
-				 * 
-				 * If we can, we should make passed/fail have a greed/red
-				 * background.
+				 * Previous todo noted above addRecords method
 				 */
 			}
 		});
-
+		
+		
 		frame.getContentPane().add(loadLog, BorderLayout.PAGE_START);
-
+		
 		JPanel bottomPanel = new JPanel();
 		frame.getContentPane().add(bottomPanel, BorderLayout.PAGE_END);
 		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
@@ -141,5 +143,77 @@ public class LogViewer {
 		});
 		bottomPanel.add(runFilter);
 		frame.getRootPane().setDefaultButton(runFilter);
+	}
+	
+	/* TODO:
+	 * 		 1. Add expressions for class, method, branches, input, and output.
+	 * 		 2. Add green/red background for pass/fail.
+	 * 		 3. Add Upper/lowercase filtering fix on logviewer.
+	 * 		 4. Add clear button for the log
+	 * 		 5. Clean up.
+	 */
+	private void addRecords(File file){
+		
+		final String IGNORE_LINE_PATTERN = "\\~";
+		final String DATE_PATTERN = "\\d{1,2}\\/\\d{1,2}\\/\\d{4}";
+		final String TIME_PATTERN = "\\d{2}:\\d{2}";
+		final String PASS_PATTERN = "(passed|Passed)|(failed|Failed)|(pass|Pass)|(fail|Fail)";
+		
+		String date, time, pass, cls, method, branches, input, output, line;
+		date = time = pass = cls = method = branches = input = output = line = "";
+		boolean flag = false;
+		
+		
+		try{
+			Scanner scan = new Scanner(file);
+			
+			
+			while((line = scan.nextLine()) != null){
+//				System.out.println(line);
+				
+				Pattern p = Pattern.compile(IGNORE_LINE_PATTERN);
+				Matcher m = p.matcher(line);
+				while(m.find()){
+					flag = true;
+					break;
+				}
+				if(flag){
+					flag = false;
+					continue;
+				}
+				
+				p = Pattern.compile(DATE_PATTERN);
+				m = p.matcher(line);
+				while(m.find()){
+					date = m.group(0);
+//					System.out.println("Date: " + date);
+				}
+				
+				p = Pattern.compile(TIME_PATTERN);
+				m = p.matcher(line);
+				while(m.find()){
+					time = m.group(0);
+//					System.out.println("Time: " + time);
+				}
+				
+				p = Pattern.compile(PASS_PATTERN);
+				m = p.matcher(line);
+				while(m.find()){
+					pass = m.group(0);
+//					System.out.println("Status: " + pass + "\n");
+				}
+				
+				if((date + time + pass + cls + method + branches + input + output).equals(""))
+					continue;
+				model.addData(date, time, pass, cls, method, branches, input, output);
+				date = time = pass = cls = method = branches = input = output = line = "";
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not Found");
+		} catch (NoSuchElementException e2) {
+			
+		} catch(NullPointerException e3){
+			return;
+		}
 	}
 }
